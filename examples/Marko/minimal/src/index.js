40d5f81006e8bede126da249cbebc584d0e22efa
Rameez Raja <rameez@2020i.com>
2025-11-17 12:52:56 +0530

    Removed marko package

--- examples/Marko/minimal/src/index.js
+++
@@ -1,5 +0,0 @@
-import "marko/express";
-import { renderIntoElement } from "marko/components";
-import CalendarDemo from "./components/calendar-demo.marko";
-
-renderIntoElement(CalendarDemo, {}, document.getElementById("root"));
--- examples/Marko/minimal/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite";
-import marko from "@marko/vite";
-
-export default defineConfig({
-  plugins: [marko()],
-});
